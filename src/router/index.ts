7b6e19950ba4716f979190703134d8e0f6c49536
luojing <luojing@bhfae.com>
2022-06-29 23:56:18 +0800

    feat: 初始化项目

---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import {
+    createRouter,
+    createWebHashHistory,
+    RouteRecordRaw
+} from 'vue-router'
+import Home from '@/views/home.vue'
+import VueX from '@/views/vuex.vue'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/vuex',
+      name: 'Vuex',
+      component: VueX
+    },
+    {
+      path: '/axios',
+      name: 'Axios',
+      component: () => import('@/views/axios.vue') // 懒加载组件
+    }
+  ]
+  
+  const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+  })
+  
+  export default router
---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import { createStore } from 'vuex'
+
+const defaultState = {
+    count: 0
+}
+
+// 新建一个store实例
+export default createStore({
+    state() {
+        return defaultState
+    },
+    mutations: {
+        increment(state: typeof defaultState) {
+            state.count++;
+        }
+    },
+    actions: {
+        increment(context) {
+            context.commit('increment')
+        }
+    },
+    getters: {
+        double(state: typeof defaultState) {
+            return 2 * state.count
+        }
+    }
+})
