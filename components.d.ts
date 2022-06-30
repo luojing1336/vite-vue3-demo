af3b3e5ef0ea82a2d3224f22e07cd618fd12f66a
luojing <luojing@bhfae.com>
2022-06-30 23:34:45 +0800

    init: 优化deploy脚本

--- components.d.ts
+++ components.d.ts
@@ -6,7 +6,6 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Header: typeof import('./src/components/Header.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     Main: typeof import('./src/components/Main.vue')['default']
     Nav: typeof import('./src/components/Nav.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
